92922ba561125fcbeb946e73d396915243340264
Ryan Lindskog <rlindskog@Ryans-MacBook-Air.local>
2017-01-25 17:59:20 -0800

    updated feature styled-componnents

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import styled from 'styled-components'
+
+const StyledElement = styled.h1`
+  color: red;
+`
+
 export default class Home extends React.Component {
     render() {
-      return <div>Page server</div>
+      return <StyledElement>Page server</StyledElement>
     }
 }
